46bba05bdbd58d999f41eed69dc15ccc5653fb06
Sarthak Roy <sarthakroy2002@gmail.com>
2025-03-07 22:56:37 +0530

    cancunf-motcamera: Update to `V1TD35M.83-20-5`
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I184a236dd3b57d49d09cd35cf20f60f64764e96b

--- cancunf-motcamera-vendor.mk
+++ cancunf-motcamera-vendor.mk
@@ -5,6 +5,18 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/motorola/cancunf-motcamera
 
+PRODUCT_COPY_FILES += \
+    vendor/motorola/cancunf-motcamera/proprietary/vendor/etc/permissions/com.motorola.camera3.cancunf.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.cancunf.xml \
+    vendor/motorola/cancunf-motcamera/proprietary/vendor/etc/permissions/com.motorola.camera3.content.ai.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.content.ai.xml \
+    vendor/motorola/cancunf-motcamera/proprietary/vendor/etc/permissions/com.motorola.camera3.lens.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.lens.xml \
+    vendor/motorola/cancunf-motcamera/proprietary/vendor/etc/permissions/com.motorola.camera3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.xml \
+    vendor/motorola/cancunf-motcamera/proprietary/product/etc/permissions/com.motorola.moto-uirefresh.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.moto-uirefresh.xml \
+    vendor/motorola/cancunf-motcamera/proprietary/product/etc/permissions/deviceowner-configuration-com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/deviceowner-configuration-com.motorola.camera3.xml \
+    vendor/motorola/cancunf-motcamera/proprietary/product/etc/permissions/privapp-permissions-com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-com.motorola.camera3.xml \
+    vendor/motorola/cancunf-motcamera/proprietary/product/etc/sysconfig/hiddenapi-whitelist-com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hiddenapi-whitelist-com.motorola.camera3.xml \
+    vendor/motorola/cancunf-motcamera/proprietary/system/etc/permissions/com.motorola.motosignature.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.motorola.motosignature.xml \
+    vendor/motorola/cancunf-motcamera/proprietary/system/etc/permissions/moto-core_services.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/moto-core_services.xml \
+    vendor/motorola/cancunf-motcamera/proprietary/system/etc/permissions/moto-settings.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/moto-settings.xml
 
 PRODUCT_PACKAGES += \
     MotoSignatureApp \
@@ -14,15 +26,4 @@ PRODUCT_PACKAGES += \
     com.motorola.motosignature \
     moto-core_services \
     moto-settings \
-    com.motorola.motosignature.xml \
-    moto-core_services.xml \
-    moto-settings.xml \
-    com.motorola.camera3.cancunf.xml \
-    com.motorola.camera3.content.ai.xml \
-    com.motorola.camera3.lens.xml \
-    com.motorola.camera3.xml \
-    com.motorola.moto-uirefresh.xml \
-    deviceowner-configuration-com.motorola.camera3.xml \
-    privapp-permissions-com.motorola.camera3.xml \
-    hiddenapi-whitelist-com.motorola.camera3.xml \
     motsettings
